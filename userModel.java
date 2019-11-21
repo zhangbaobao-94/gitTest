c4f4a16cf4469053bf5b22a94c16f0898806fdfb
Amber <huchen94@163.com>
2019-11-21 20:45:27 +0800

    修改dev中的userModel

--- userModel.java
+++ userModel.java
@@ -1,6 +1,8 @@
 public class userModel{
 	public static void main(String[] args){
 		String ss = "测试删除";
+        String dd = "莫得办法, 年龄大了没法说";
+        String mm = "随缘吧";
 		System.out.println(ss);
 	}
 }
