095a6e0325cdf3d5ec498df421f5a36d8d91462f
zhangdan <1049270362@qq.com>
2019-12-12 20:52:58 +0800

    12/12修改

--- alibug.java
+++ alibug.java
@@ -2,5 +2,7 @@ public class alibug{
 	public static void main(String[] args){
 		String ss = "看看分支实际上是这样用的";
 		
+		String s = "哈哈";
+		
 	}
 }
--- wechat.java
+++ wechat.java
@@ -4,5 +4,8 @@ public class wechat{
 		int bb = 14;
 		int c = aa + bb;
 		System.out.println(c);
+		
+		String a = "哈哈";
+		System.out.println(a);
 	}
 }
